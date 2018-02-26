3ee21d659296b9956ff1f3d22eb7c5b3a9059281
Cristopher Hernandez <DoughboysWWI@gmail.com>
2018-02-25 22:44:05 -0800

    Minor fixes and command line input

--- lexer.cpp
+++ lexer.cpp
@@ -8,14 +8,14 @@ lexer::~lexer() {
     //destructor
 }
 
-bool lexer::is_identifier(const string& token) {
+bool lexer::is_identifier(const std::string& token) {
     //finite state machine check for identifier
 }
 
-bool lexer::is_integer(const string& token) {
+bool lexer::is_integer(const std::string& token) {
     //finite state machine check for integer
 }
 
-bool lexer::is_real(const string& token) {
+bool lexer::is_real(const std::string& token) {
     //finite state machine check for real
 }
--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
+#include <string>
 #include "lexer.h"
 using namespace std;
 
 //TODO: grab filename from command line (or other input) and run logic
 
-int main() {
-    cout << "Program begin\n";
+int main(int argc, char** argv) {
+    lexer rat18s_lex = lexer();
+    string file_to_process;
+
+    cout << "Program begin" << endl;
+    if (argc < 2) {
+        cout << "USAGE: <program calling name> <file name>" << endl;
+    }
+    else {
+        file_to_process = string(argv[1]);
+        cout << "File to process: " << argv[1] << endl;
+        //do stuff
+    }
 
     return 0;
 }
