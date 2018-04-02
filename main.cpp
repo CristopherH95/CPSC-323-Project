4f3ffb961fce0e4ced59de7e3a2abaf024f7aa01
Cristopher Hernandez <DoughboysWWI@gmail.com>
2018-04-01 19:36:37 -0700

    Changed main to include parser

--- main.cpp
+++ main.cpp
@@ -2,29 +2,32 @@
 #include <fstream>
 #include <string>
 #include "lexer.h"
+#include "parser.h"
 using namespace std;
 
-//TODO: update main to reflect addition of parser class
+//main is currently configured to output only parser data
 int main(int argc, char** argv) {
     lexer rat18s_lex = lexer();
+    parser rat18s_par = parser();
     string file_to_process;
     string target_file;
     fstream input_file;
     ofstream output_file;
 
     if (argc < 3) {
-        cout << "USAGE: rat18s_lexer <input file name> <output file name>" << endl;
+        cout << "USAGE: " << argv[0] << " <input file name> <output file name>" << endl;
     }
     else {
         cout << "Starting lexical analyzer. . ." << endl;
         file_to_process = string(argv[1]);
         target_file = string(argv[2]);
         cout << "Processing file: " << file_to_process << endl;
-        cout << "Saving tokens to file: " << target_file << endl;
+        cout << "Saving output to file: " << target_file << endl;
         input_file.open(file_to_process);
         rat18s_lex.process_file(input_file);
+        rat18s_par.initialize_parse();
         output_file.open(target_file);
-        rat18s_lex.print_tokens(output_file);
+        rat18s_par.parse(rat18s_lex, output_file);
         output_file.close();
         input_file.close();
         cout << "Done." << endl;
